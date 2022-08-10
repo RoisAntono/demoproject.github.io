f1a9a767047c0dafe993ccd9a3b90b02f2082391
RoisAntono <67670208+RoisAntono@users.noreply.github.com>
2022-08-10 12:19:18 +0700

    update

--- application/modules/u_denah/views/V_denah.php
+++
@@ -1,4 +0,0 @@
-<div class="section-title position-relative pb-3 mb-5">
-    <h3 class="fw-bold text-primary text-uppercase">Denah Sekolah</h3>
-</div>
-<img class="img-fluid w-100 rounded mb-5" src="assets/user/img/Screenshot_95.jpg" alt="">
--- application/modules/u_prestasi/views/V_prestasi.php
+++
@@ -1,3 +0,0 @@
-<div class="section-title position-relative pb-3 mb-5">
-    <h3 class="fw-bold text-primary text-uppercase">Prestasi Siswa</h3>
-</div>
